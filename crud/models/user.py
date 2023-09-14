bf68032f2876d4a9c9dcb2afe4b4bddb535723c6
PleahMaCaka <pleahmacaka@gmail.com>
2023-09-14 16:55:30 +0900

    fix: avoid the same name with pw and property method

--- crud/models/user.py
+++ crud/models/user.py
@@ -8,18 +8,18 @@ from app import db
 class User(db.Model):
     __tablename__ = "users"
 
+    # column
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, index=True)
     email = db.Column(db.String, unique=True, index=True)
-    password = db.Column(db.String)
+    password_hash = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
-    # noinspection PyRedeclaration
     @property
     def password(self):
-        raise AttributeError("Password is not a readable attribute")
+        raise AttributeError("Password is not a readable attribute.")
 
     @password.setter
-    def password(self, pw):
-        self.password = generate_password_hash(pw)
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
